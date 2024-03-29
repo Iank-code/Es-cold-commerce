abf22a6b0407fe210cadba51e7cf416f25064ac7
Ian Kamau <kian99564@gmail.com>
2024-03-29 12:40:30 +0300

    Feat: delete request for category implemented

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -4,5 +4,6 @@ const authenticateRequest = require("../validators/auth.validators.js");
 
 router.post("/create", authenticateRequest(), CategoryController.addCategory);
 router.get("/all", authenticateRequest(), CategoryController.getAllCategories);
+route.delete("/:id", authenticateRequest(), CategoryController.deleteCategory);
 
 module.exports = router;
